c0daee79e8bb658b24f88a5ee7220670e2b3e1f2
fitDizzle <markfitnessaz@gmail.com>
2022-04-14 22:53:44 -0700

    added procfile

--- src/App.js
+++ src/App.js
@@ -5,29 +5,26 @@ import SocialMediaLinks from './components/navigation/socialMediaLinks';
 
 function App() {
   return (
-      <div className="App">
+    <div className="App">
 
-        <header id="header">
-          <div className="container">
+      <header id="header">
+        <div className="container">
 
-            <h1><a href="index.html">Mark Clark</a></h1>
+          <h1><a href="index.html">Mark Clark</a></h1>
 
-            <h2>I am a <span>web developer</span> in Phoenix, Arizona</h2>
+          <h2>I am a <span>web developer</span> in Phoenix, Arizona</h2>
 
-            <Navbar />
-            <SocialMediaLinks />
-          </div>
-        </header>
-
-        <About />
-
-        <div className="credits">
-          {/* <!-- You can delete the links only if you purchased the pro version. -->
-    <!-- Licensing information: https://bootstrapmade.com/license/ -->
-    <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/personal-free-resume-bootstrap-template/ -->
-    Mark Clark <a href="https://bootstrapmade.com/">| Dragon Cats</a> */}
+          <Navbar />
+          <SocialMediaLinks />
         </div>
+      </header>
+
+      <About />
+
+      <div className="credits">
+        Mark Clark <a href="https://bootstrapmade.com/">| Dragon Cats</a>
       </div>
+    </div>
   );
 }
 
